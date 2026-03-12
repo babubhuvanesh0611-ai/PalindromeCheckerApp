d3ec3a1b4e604627953c926774ab0f0e49241e9f
babubhuvanesh0611-ai <babubhuvanesh0611@gmail.com>
2026-03-12 21:40:04 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,60 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
+
+
+ 
+public class UseCase11PalindromeCheckerApp {
+
+    /*
+     * Application entry point
+     */
     public static void main(String[] args) {
-        System.out.println("Hello PalindromeCheckerApp");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Create object of service class
+        PalindromeService service = new PalindromeService();
+
+        // Call method
+        boolean result = service.checkPalindrome(input);
+
+        if (result) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
+
+
+/*
+
+ */
+class PalindromeService {
+
+    /*
+     * Method to check palindrome
+     */
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        // Compare characters moving inward
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }

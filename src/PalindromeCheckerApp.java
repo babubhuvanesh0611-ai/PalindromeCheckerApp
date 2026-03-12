cf0078a2be729c1a4df8e11e51190c51ff8fefe7
babubhuvanesh0611-ai <babubhuvanesh0611@gmail.com>
2026-03-12 20:41:19 +0530

    Refactor PalindromeCheckerApp to UseCase4PalindromeCheckerApp

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,39 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
+
+public class UseCase4PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello PalindromeCheckerApp");
+
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        
+        char[] chars = input.toCharArray();
+
+        
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
     }
 }

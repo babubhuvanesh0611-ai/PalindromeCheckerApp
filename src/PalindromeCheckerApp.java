7eaa42dacf474bf51254fa85b130b99fe7d62d71
babubhuvanesh0611-ai <babubhuvanesh0611@gmail.com>
2026-03-12 20:49:51 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,46 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class UseCase6PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello PalindromeCheckerApp");
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input from user
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Create Stack and Queue
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Push to stack and enqueue to queue
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            stack.push(ch);
+            queue.add(ch);
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare dequeue from queue and pop from stack
+        while (!stack.isEmpty()) {
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
     }
 }

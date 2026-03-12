fdce0090753a2fa65872f4b5c16b01b8e170abba
babubhuvanesh0611-ai <babubhuvanesh0611@gmail.com>
2026-03-12 10:43:51 +0530

    UC1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,12 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("Hello PalindromeCheckerApp");
-    }
+
+       public static void main(String[] args) {
+
+            System.out.println("=================================");
+            System.out.println("Welcome to  PalindromeCheckerApp");
+            System.out.println("PalindromeCheckerApp");
+            System.out.println("System initialized successfully");
+            System.out.println("=================================");
+
+        }
 }

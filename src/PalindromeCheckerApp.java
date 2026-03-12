95ef4c745a65559b0c5fd30ffac7e22583587e78
babubhuvanesh0611-ai <babubhuvanesh0611@gmail.com>
2026-03-12 10:49:46 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,27 @@
-public class PalindromeCheckerApp {
+package PackageCheckerApp;
+
+public class PlanidromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello PalindromeCheckerApp");
+
+        // Hardcoded string literal
+        String word = "madam";
+
+        // Check if the string is a palindrome
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Conditional statement to print result
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
     }
 }
